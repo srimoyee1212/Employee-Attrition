e0dcd020de8ab1a1bce03635775803c878af6a89
srimoyee1212 <30791239+srimoyee1212@users.noreply.github.com>
2020-05-27 10:18:37 +0530

    preprocessig the data

---
+++ preprocessor.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import DataFrame
+
+
+existing=pd.read_excel('/home/srimoyee/Desktop/Srimoyee-PROJECT_REPORT/stage1/TakenMind-Python-Analytics-Problem-case-study-1-1.xlsx', sheet_name='Existing employees')
+left= pd.read_excel('/home/srimoyee/Desktop/Srimoyee-PROJECT_REPORT/stage1/TakenMind-Python-Analytics-Problem-case-study-1-1.xlsx', sheet_name='Employees who have left')
+
+existing['left']=0
+
+left['left']=1
+
+print(existing.dtypes)
+print(left.dtypes)
+
+frames=[existing,left]
+res=pd.concat(frames)
+
+
+res=res.sort_values('Emp ID')
+#print(res)
+
+writer = ExcelWriter('/home/srimoyee/Desktop/Srimoyee-PROJECT_REPORT/stage1/Merged_data.xlsx')
+res.to_excel(writer,'Merged',index=False)
+writer.save()
